bfd4f9f1cf656bbb2c75df60602d7737530407b2
Sahil <128493339+Ranasahil19@users.noreply.github.com>
2025-02-16 14:25:09 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,29 @@
+let score = "sahil"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+let isLoggedIn = ""
+let valueInBoolean = Boolean(isLoggedIn)
+let valueInString = String(isLoggedIn)
+
+console.log(valueInBoolean);
+
+console.log(typeof valueInString);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "sahil" => true
+
+
