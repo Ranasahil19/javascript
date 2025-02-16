6ff271e50c7fb5f61f068b392ab3f1c476662325
Sahil <128493339+Ranasahil19@users.noreply.github.com>
2025-02-16 11:50:05 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144552
+let accountEmail = "sahil@gmail.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+let accountState;
+
+// accountId = 1332
+accountEmail = "sr@gm.com"
+accountPassword = "121212"
+accountCity = "Goa"
+
+console.table([accountId , accountEmail , accountPassword , accountCity , accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+//alert("hello") //we are use nodeJS not browser
+
+console.log(3 + 3);
+console.log("sahil");
+
+let name = "sahil"
+let age = 19
+let isloggedIn = false
+
+//  number => 2 to power 53
+//  bigint
+//  string => ""
+//  boolean => true/false
+//  null => standalone value
+//  undefined =>
+//  symbol => unique
+
+//  object
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+
+
