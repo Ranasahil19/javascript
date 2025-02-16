c7ed6f9b0e70ecd223135a61b87f80946ce8900c
Sahil <128493339+Ranasahil19@users.noreply.github.com>
2025-02-16 15:06:14 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "sahil"
-console.log(typeof score);
-console.log(typeof(score));
+// console.log(typeof score);
+// console.log(typeof(score));
 
 let valueInNumber = Number(score)
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -17,9 +17,9 @@ let isLoggedIn = ""
 let valueInBoolean = Boolean(isLoggedIn)
 let valueInString = String(isLoggedIn)
 
-console.log(valueInBoolean);
+//console.log(valueInBoolean);
 
-console.log(typeof valueInString);
+// console.log(typeof valueInString);
 
 // 1 => true
 // 0 => false
@@ -27,3 +27,52 @@ console.log(typeof valueInString);
 // "sahil" => true
 
 
+//************************************** Oprations ****************************************
+
+let value = 3;
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%3);
+// console.log(2/3);
+
+let str1 = "hello "
+let str2 = "sahil"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(true);
+// console.log(+"");
+
+let num1, num2, num3;
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter);
+
+let x = 3;
+const y = x++;//postprix
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;//prefix
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
