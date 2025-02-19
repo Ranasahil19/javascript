d161f75b8ee509c9969c8e61a630ca287761f620
Sahil <128493339+Ranasahil19@users.noreply.github.com>
2025-02-19 18:16:21 +0000

    function

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -4,11 +4,50 @@ if(true){
     let a = 10
     const b = 20
     // var c = 30
-    console.log("inner" , a);
+    // console.log("inner" , a);
     
 }
 
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "sahil"
+
+    function two(){
+        const website = "youtube"
+        // console.log(username);
+        
+    }
+    // console.log(website);
+    two()
+}
+
+one()
+
+if(true){
+    const username = "sahil"
+    if(username === "sahil"){
+        const website= " youtube"
+        // console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+// console.log(username);
+
+
+//   interesting   //
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+addtwo(5)
+const addtwo = function(num){
+    return num + 2
+} 
 
