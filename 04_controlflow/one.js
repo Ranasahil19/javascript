ab0d27fb7394cbfd791b1d5ce6d00ac037052c2a
Sahil <128493339+Ranasahil19@users.noreply.github.com>
2025-02-22 14:14:18 +0000

    control flow

--- 04_controlflow/one.js
+++ 04_controlflow/one.js
@@ -41,8 +41,14 @@
 
 const userLoggedIn = true
 const debitCard = true
-const loggedIn
+const loggedInFromGoogle = false
+const loggedInFromEmail = true
+
 if(userLoggedIn && debitCard && 2==3){
     console.log("Allow to buy course");
-    
 }
+
+if(loggedInFromGoogle || loggedInFromEmail){
+    console.log("User logged In");    
+}
+
