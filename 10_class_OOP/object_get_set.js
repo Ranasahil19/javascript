336f1330e4a58e226304c5d8b271f72b9aa29f26
Sahil <128493339+Ranasahil19@users.noreply.github.com>
2025-03-08 12:08:08 +0000

    get set

--- 10_class_OOP/object_get_set.js
+++ 10_class_OOP/object_get_set.js
@@ -3,7 +3,7 @@ const User = {
     _password : 'sahil123',
 
     get email(){
-        return this._email.toUpperCase()
+        return this._email.toUpperCasxxe()
     },
     set email(value){
         this._email = value
